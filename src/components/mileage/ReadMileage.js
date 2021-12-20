c810c73fb49e3da1b33dac0312db0558d282d3cf
Joseph Tran <fred.smoth@gmail.com>
2021-12-20 18:51:53 +1100

    Reading data works

--- src/components/mileage/ReadMileage.js
+++ src/components/mileage/ReadMileage.js
@@ -1,3 +1,24 @@
+import { useLiveQuery } from "dexie-react-hooks";
+import { db } from "./../../db/DB";
+
 export default function ReadMileage() {
-  return <div>ReadMileage</div>;
+  const storedData = useLiveQuery(() => {
+    // get the most recent events
+    return db.mileage.orderBy("timeUtc").reverse().limit(5).toArray();
+  });
+
+  console.log(storedData);
+
+  if (!storedData) return null;
+
+  return (
+    <div>
+      {storedData?.map((entry) => (
+        <div key={entry.id}>
+          {entry.carId} {entry.currentMileage}{" "}
+          {new Date(entry.timeUtc).toISOString()}
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/db/DB.js
+++ src/db/DB.js
@@ -2,5 +2,5 @@ import Dexie from "dexie";
 
 export const db = new Dexie("fit_car");
 db.version(1).stores({
-  mileage: "++id, car_id, current_mileage, time_utc",
+  mileage: "++id, carId, currentMileage, timeUtc",
 });
